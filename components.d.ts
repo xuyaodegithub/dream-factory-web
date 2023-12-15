64bc34d2089ceef2da85932ade4e9e6a9a4f8cda
WinnerCC <61073390+1999lcy@users.noreply.github.com>
2023-12-15 18:01:59 +0800

    Friday
    修改了导航栏的样式

--- components.d.ts
+++ components.d.ts
@@ -9,11 +9,21 @@ declare module 'vue' {
   export interface GlobalComponents {
     AAvatar: typeof import('ant-design-vue/es')['Avatar']
     AButton: typeof import('ant-design-vue/es')['Button']
+    ADropdown: typeof import('ant-design-vue/es')['Dropdown']
+    ALayout: typeof import('ant-design-vue/es')['Layout']
+    ALayoutHeader: typeof import('ant-design-vue/es')['LayoutHeader']
     AMenu: typeof import('ant-design-vue/es')['Menu']
     AMenuItem: typeof import('ant-design-vue/es')['MenuItem']
     ASpace: typeof import('ant-design-vue/es')['Space']
-    Navbar: typeof import('./src/components/Navbar.vue')['default']
+    IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
+    IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
+    IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
+    IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
+    IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
+    Navbar: typeof import('./src/components/Navbar/Navbar.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
+    WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
   }
 }
---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,74 @@
+<template>
+    <a-layout>
+        <a-layout-header :style="{ display: 'flex', position: 'fixed', zIndex: 1, width: '100%' }">
+            <div class="logo">
+                <!-- <img src="@/assets/images/logo.png" alt="LOGO" :style="{ height: '100%' }" /> -->
+            </div>
+            <a-menu v-model:selectedKeys="current" theme="dark" mode="horizontal"
+                :style="{ flex: '1', lineHeight: '64px' }">
+                <a-menu-item key="homepage">首页</a-menu-item>
+                <a-menu-item key="modelReplace">模特替换</a-menu-item>
+            </a-menu>
+            <div class="right-menu">
+                <a-space :size="25">
+                    <span class="account-balance">账户余额: 3000算力</span>
+                    <a-button>购买</a-button>
+                    <a-dropdown>
+                        <a-avatar size="large" style="background-color: #87d068">
+                            <template #icon>
+                                <UserOutlined />
+                            </template>
+                        </a-avatar>
+                        <template #overlay>
+                            <a-menu>
+                                <a-menu-item>
+                                    <user-outlined />
+                                    <a href="javascript:;">1st menu item</a>
+                                </a-menu-item>
+                                <a-menu-item>
+                                    <a href="javascript:;">2nd menu item</a>
+                                </a-menu-item>
+                                <a-menu-item>
+                                    <a href="javascript:;">3rd menu item</a>
+                                </a-menu-item>
+                            </a-menu>
+                        </template>
+                    </a-dropdown>
+
+                </a-space>
+            </div>
+        </a-layout-header>
+    </a-layout>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { UserOutlined } from '@ant-design/icons-vue';
+export default defineComponent({
+    components: {
+        UserOutlined
+    },
+    setup() {
+        const current = ref<string[]>(['homepage']);
+        return {
+            current,
+        };
+    },
+});
+</script>
+<style lang="less">
+.logo {
+    width: 120px;
+    height: 31px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 16px 24px 16px 0;
+    float: left;
+}
+
+.right-menu {
+    margin-left: auto;
+}
+
+.account-balance {
+    color: rgb(250, 250, 250);
+}
+</style>
